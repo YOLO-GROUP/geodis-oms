bf45ef4f5deb218878dae2fa23783654eef2b5cb
Lê Quang Phương <quangphuongle1992@gmail.com>
2016-11-28 22:28:09 +0700

    login prototype gui

--- OMS_PORTAL/resources/assets/js/login/login.js
+++ OMS_PORTAL/resources/assets/js/login/login.js
@@ -1,8 +1,8 @@
 require('../bootstrap');
-require('../libs/TweenMax.min.js');
+// require('../libs/TweenMax.min.js');
 
 $(document).ready(function () {
-    $(document).mousemove(function (event) {
-        TweenLite.to($('body'), .5, {css: {'background-position': parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / 12) + "px, " + parseInt(event.pageX / 15) + "px " + parseInt(event.pageY / 15) + "px, " + parseInt(event.pageX / 30) + "px " + parseInt(event.pageY / 30) + "px"}});
-    });
+    // $(document).mousemove(function (event) {
+    //     TweenLite.to($('body'), .5, {css: {'background-position': parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / 12) + "px, " + parseInt(event.pageX / 15) + "px " + parseInt(event.pageY / 15) + "px, " + parseInt(event.pageX / 30) + "px " + parseInt(event.pageY / 30) + "px"}});
+    // });
 });
